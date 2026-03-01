24aac420cb670a288368d96fa01c7ff2971189ac
Alex Wright <alexwmw@gmail.com>
2026-03-01 13:28:36 +0000

    Init

---
+++ eslint.config.js
@@ -0,0 +1,94 @@
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint-define-config";
+import importPlugin from "eslint-plugin-import";
+import jsdoc from "eslint-plugin-jsdoc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import promisePlugin from "eslint-plugin-promise";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import security from "eslint-plugin-security";
+import sonarjs from "eslint-plugin-sonarjs";
+import unicorn from "eslint-plugin-unicorn";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    ignores: ["dist", "build", "node_modules"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2024,
+        ...globals.webextensions,
+        ...globals.serviceworker,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      import: importPlugin,
+      "unused-imports": unusedImports,
+      promise: promisePlugin,
+      sonarjs,
+      unicorn,
+      security,
+      jsdoc,
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      /* React */
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "react/no-unstable-nested-components": "error",
+      "react/jsx-no-leaked-render": "error",
+
+      /* General JS/TS */
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+      curly: ["error", "all"],
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+
+      /* Imports */
+      "unused-imports/no-unused-imports": "error",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+
+      /* Performance & Security */
+      "security/detect-object-injection": "off", // Usually too noisy
+      "security/detect-non-literal-fs-filename": "warn",
+
+      /* TypeScript rules */
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      /* Extension Specific */
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+]);
