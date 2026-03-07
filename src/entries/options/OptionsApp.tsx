d45b1fcb7f4a0a59456b504a03dc68f5c5f27c9f
Alex Wright <alexwmw@gmail.com>
2026-03-07 15:22:26 +0000

    Add basic app implementation

--- src/entries/options/OptionsApp.tsx
+++ src/entries/options/OptionsApp.tsx
@@ -1,16 +1,32 @@
+import { useCallback } from 'react';
+
 import useBlockRules from '../../hooks/useBlockRules';
+import type { BlockRule } from '../../types/schema';
 
 const OptionsApp = () => {
-  const { blockRules } = useBlockRules();
+  const { blockRules, removeRule } = useBlockRules();
+
+  // Basic implementation for testing - allows easy viewing and removal of a stored rules
+  const mapRules = useCallback(
+    (rule: BlockRule) => {
+      const handleRemove = () => {
+        removeRule(rule.id).catch(console.error);
+      };
+      return (
+        <li key={rule.id}>
+          {rule.pattern}
+          <button onClick={handleRemove}>Remove</button>
+        </li>
+      );
+    },
+    [removeRule],
+  );
+
   return (
     <div>
       <p>Hello, world!</p>
       {blockRules?.length ? <h2>Block Rules</h2> : <h2>No Block Rules</h2>}
-      <ul>
-        {blockRules?.map((rule) => (
-          <li key={rule.id}>{rule.pattern}</li>
-        ))}
-      </ul>
+      <ul>{blockRules?.map(mapRules)}</ul>
     </div>
   );
 };
--- src/entries/popup/PopupApp.tsx
+++ src/entries/popup/PopupApp.tsx
@@ -1,5 +1,66 @@
+import { useEffect, useState } from 'react';
+
+import useBlockRules from '../../hooks/useBlockRules';
+import { BlockService } from '../../services/BlockService';
+import type { BlockRule } from '../../types/schema';
+
+// temporary hook just to get basic functionality
+const useActiveTab = () => {
+  const [activeTab, setActiveTab] = useState<chrome.tabs.Tab | null>();
+  const [isSupported, setIsSupported] = useState<boolean>(false);
+
+  useEffect(() => {
+    const queryOptions: chrome.tabs.QueryInfo = { active: true, lastFocusedWindow: true };
+    chrome.tabs
+      .query(queryOptions)
+      .then((tabs) => {
+        if (tabs && tabs.length > 0) {
+          setActiveTab(tabs[0]);
+        }
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    activeTab && activeTab.url && setIsSupported(BlockService.isSupportedUrl(activeTab.url));
+  }, [activeTab]);
+
+  return { activeTab, isSupported };
+};
+
 const PopupApp = () => {
-  return <div />;
+  const { activeTab, isSupported } = useActiveTab();
+  const { addRule } = useBlockRules();
+
+  // Basic implementation for testing - allows easy adding of rule
+  const handleAddRuleClick = () => {
+    if (!activeTab || !isSupported) {
+      return;
+    }
+    const pattern = activeTab?.url?.split('//').pop();
+    if (!pattern) {
+      return;
+    }
+    const rule: BlockRule = {
+      id: 'test',
+      matchType: 'domain',
+      pattern,
+      createdAt: new Date().toISOString(),
+      enabled: true,
+    };
+    addRule(rule).catch(console.error);
+  };
+
+  return (
+    <div>
+      <button
+        disabled={!activeTab || !isSupported}
+        onClick={handleAddRuleClick}
+      >
+        Add Site
+      </button>
+    </div>
+  );
 };
 
 export default PopupApp;
