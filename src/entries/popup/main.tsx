0b79d135259403b89ae3c44e21d3829191b1661b
Alex Wright <awrigh15@dcs.bbk.ac.uk>
2026-03-17 20:26:29 +0000

    Revamp popup status UI with shared primitives

--- src/entries/popup/main.tsx
+++ src/entries/popup/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
+import '../shared/theme.css';
+
 import PopupApp from './PopupApp';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
