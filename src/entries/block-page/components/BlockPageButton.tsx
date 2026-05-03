24f2b6e0f3d80ee1519efd1c1d83bd643d29803a
Alex Wright <alexwmw@gmail.com>
2026-05-03 22:06:29 +0100

    Add completion animation

--- src/entries/block-page/components/BlockPageButton.tsx
+++ src/entries/block-page/components/BlockPageButton.tsx
@@ -3,6 +3,7 @@ import type { KeyboardEventHandler, MouseEventHandler } from 'react';
 
 import styles from './BlockPageButton.module.css';
 import ButtonOutline from './buttonOutline.svg?react';
+import Completion from './Completion';
 
 import Title from '@/assets/icons/icon-white-on-bg.svg?react';
 
@@ -19,15 +20,19 @@ export type HoldButtonProps = {
 const HoldButton = (props: HoldButtonProps) => {
   const { autoFocus, animationDuration, held, holdIsComplete, remainingTime, onKeyDown, onMouseDown } = props;
   const buttonText =
-    remainingTime === null ? <Title title='Hold' /> : <span>{remainingTime === 0 ? 'OK' : remainingTime}</span>;
+    remainingTime === null ? <Title title='Hold' /> : <span>{remainingTime === 0 ? '' : remainingTime}</span>;
 
   return (
     <div className={styles.holdAction}>
       <div className={styles.buttonStack}>
-        <ButtonOutline
-          className={clsx(styles.buttonEdge, held && styles.held)}
-          style={{ animationDuration: animationDuration + 's' }}
-        />
+        {holdIsComplete ? (
+          <Completion className={styles.completionAnimation} />
+        ) : (
+          <ButtonOutline
+            className={clsx(styles.buttonEdge, held && styles.held)}
+            style={{ animationDuration: animationDuration + 's' }}
+          />
+        )}
         <button
           autoFocus={autoFocus}
           onKeyDown={onKeyDown}
